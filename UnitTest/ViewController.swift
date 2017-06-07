d1c1541b59c8f9548d7d07bf53906b5cc9fa39e2
Maksim Isaev <maxisa@Maksims-MacBook-Pro.local>
2017-06-07 17:16:50 +0300

    Initial Commit

---
+++ UnitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTest
+//
+//  Created by Maksim Isaev on 07/06/2017.
+//  Copyright © 2017 Maksim Isaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
